8889879564896ac52734fcd81aa3c540c6f5d474
Seli888 <s.d.selivanov@yandex.ru>
2025-03-22 23:59:00 +0700

    zadacha2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,18 @@
 package org.example;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String filePath = "literatura.txt";
 
-        System.out.println("Введите 2 числа:");
-
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
-
-        Calculate calculate = new Calculate();
-
-        try{
-            System.out.println(calculate.calc(number1, number2));
-        }catch (ArithmeticException e){
-            System.out.println("делить на 0 нельзя");
-            scanner.close();
-        }finally {
-            scanner.close();
+        try{ Scanner scanner = new Scanner(new File(filePath));
+        } catch (FileNotFoundException e){
+            System.out.println("файл не найден");
+        } finally{
+            System.out.println("Работа с файлом завершена");
         }
     }
 }
