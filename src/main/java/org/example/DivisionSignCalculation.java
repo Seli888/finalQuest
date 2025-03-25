3073a149d656c849ed7ac9ba4935592b422d3253
Seli888 <s.d.selivanov@yandex.ru>
2025-03-25 18:17:40 +0700

    finally

--- src/main/java/org/example/DivisionSignCalculation.java
+++ src/main/java/org/example/DivisionSignCalculation.java
@@ -4,6 +4,19 @@ import java.math.BigDecimal;
 
 public class DivisionSignCalculation implements Calculation {
     public BigDecimal calculate(double operand1, double operand2) {
+
+        try {
+             BigDecimal.valueOf(operand1).divide(BigDecimal.valueOf(operand2));
+        } catch (NegativeNumberExeption e){
+            System.out.println("Деление на 0 не приветствуется");
+        }
         return BigDecimal.valueOf(operand1).divide(BigDecimal.valueOf(operand2));
+
     }
+       public static void check(double operand2) {
+           if (operand2 == 0) {
+               throw new NegativeNumberExeption();
+           }
+    }
+
 }
