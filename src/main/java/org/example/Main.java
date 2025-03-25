2290b1b94f2efaca13de1f10dff8f13c783cee2d
Seli888 <s.d.selivanov@yandex.ru>
2025-03-25 16:13:57 +0700

    zadacha2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,20 @@
 package org.example;
-import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        boolean per = false;
+        String filePath = "C:\\Users\\django\\Desktop\\go.txt";
 
-        while (!per) {
-            try {
-                System.out.println("Введите 2 числа:");
-
-                int number1 = scanner.nextInt();
-                int number2 = scanner.nextInt();
-
-                calc(number1, number2);
-                per = true;
-            } catch (ArithmeticException e) {
-                System.out.println("делить на 0 нельзя");
-            }
+        try{ String text = new String(Files.readAllBytes(Paths.get(filePath)));
+                System.out.println(text);
+        } catch (IOException e){
+                System.out.println("файл не найден");
+        } finally{
+                System.out.println("Работа с файлом завершена");
         }
-    }
-    public static void calc(int number1, int number2){
-        System.out.println(number1 / number2);
     }
 }
